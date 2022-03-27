44d801752723aa009d1ad5d7190728daa80c502d
Luiz Gustavo <100114431+lzcaetano@users.noreply.github.com>
2022-03-27 00:44:13 -0300

    App que mostra as modificações na borda de um app

---
+++ Random apps/BorderRadiusPreviewer/script.js
@@ -0,0 +1,41 @@
+function changeSquareColor(value) {
+
+    if (value == 1) {
+        percentage = "0%";
+        document.querySelector("#example-square").style.borderTopLeftRadius = percentage;
+    } else if (value == 2) {
+        percentage = "25%";
+        document.querySelector("#example-square").style.borderTopLeftRadius = percentage;
+    } else if (value == 3) {
+        percentage = "50%";
+        document.querySelector("#example-square").style.borderTopLeftRadius = percentage;
+    } else if (value == 4) {
+        percentage = "0%";
+        document.querySelector("#example-square").style.borderTopRightRadius = percentage;
+    } else if (value == 5) {
+        percentage = "25%";
+        document.querySelector("#example-square").style.borderTopRightRadius = percentage;
+    } else if (value == 6) {
+        percentage = "50%";
+        document.querySelector("#example-square").style.borderTopRightRadius = percentage;
+    } else if (value == 7) {
+        percentage = "0%";
+        document.querySelector("#example-square").style.borderBottomLeftRadius = percentage;
+    } else if (value == 8) {
+        percentage = "25%";
+        document.querySelector("#example-square").style.borderBottomLeftRadius = percentage;
+    } else if (value == 9) {
+        percentage = "50%";
+        document.querySelector("#example-square").style.borderBottomLeftRadius = percentage;
+    } else if (value == 10) {
+        percentage = "0%";
+        document.querySelector("#example-square").style.borderBottomRightRadius = percentage;
+    } else if (value == 11) {
+        percentage = "25%";
+        document.querySelector("#example-square").style.borderBottomRightRadius = percentage;
+    } else if (value == 12) {
+        percentage = "50%";
+        document.querySelector("#example-square").style.borderBottomRightRadius = percentage;
+    }
+
+}
